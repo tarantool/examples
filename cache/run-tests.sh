515d7bb03ed7e9fbc84432c5dd6bc05f9c646e64
artem.morozov <artembo@me.com>
2020-02-25 16:10:54 +0300

    added mysql for integrations tests in ci

---
+++ cache/run-tests.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+luatest ./test/unit
+luatest ./test/integration/cache_mysql_test.lua
+luatest ./test/integration/cache_vinyl_test.lua
+luatest ./test/integration/simple_cache_test.lua
+luatest ./test/integration/api_test.lua
