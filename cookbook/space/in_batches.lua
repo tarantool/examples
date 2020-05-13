6deb8bf3026a8b2dcf5f6ae557cabde1939f5136
Max Melentiev <melentievm@gmail.com>
2020-05-13 19:08:51 +0300

    Add example for in_batches cookbook (#16)
    * Add example for in_batches cookbook
    * Add note about old Tarantool versions to alter_field_type cookbook

--- cookbook/space/in_batches.lua
+++ cookbook/space/in_batches.lua
@@ -49,4 +49,66 @@ function export.yield_every(batch_size, iter)
     end)
 end
 
+function export.example(batch_size, space_name)
+    batch_size = batch_size or 100
+
+    -- Create space and add initial data.
+    local space = box.schema.space.create(space_name or 'in_batches_example')
+    space:format({
+        {'key', 'number'},
+        {'value', 'number'},
+    })
+    space:create_index('primary', {parts = {'key'}})
+    for i = 1, 1010 do space:insert({i, i}) end
+
+    -- Helper function to print the number of yields, to ensure that they actually occur.
+    local function count_yields(fn)
+        local yields = 0
+        local counter = fiber.new(function()
+            while true do
+                fiber.yield()
+                yields = yields + 1
+            end
+        end)
+        local result = fn()
+        counter:cancel()
+        print('yielded: ' .. yields)
+        return result
+    end
+
+    -- Use `atomic` to update all tuples.
+    --
+    -- It yields once for every batch and one more time for the last partial batch.
+    -- Same as `math.ceil(count / batch_size)`
+    --
+    -- However, there can be more yields because multiple ones may occur during
+    -- long WAL write.
+    count_yields(function()
+        export.atomic(batch_size, space:pairs(), function(tuple)
+            space:update(tuple.key, {{'=', 'value', math.random(10)}})
+        end)
+    end)
+
+
+    -- Here is an analog of the SQL query:
+    --      SELECT value, COUNT(*)
+    --      FROM space
+    --      WHERE MOD(key % 100) != 0
+    --      GROUP BY value
+    --
+    -- As the query is read-only, we can use `yield_every` with a chain
+    -- of iterator transformations.
+    --
+    -- It yields only `math.floor(count / batch_size)` times because there is
+    -- no yield after the final partial batch.
+    return count_yields(function()
+        return export.yield_every(batch_size, space:pairs()):
+            filter(function(tuple) return tuple.key % 100 ~= 0 end):
+            reduce(function(acc, tuple)
+                acc[tuple.value] = (acc[tuple.value] or 0) + 1
+                return acc
+            end, {})
+    end)
+end
+
 return export
