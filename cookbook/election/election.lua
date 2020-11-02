5b705c0e69e30493703fd7d9d467e3324a636139
Serge Petrenko <sergepetrenko@tarantool.org>
2020-11-02 11:07:15 +0300

    Leader election example fixes

--- cookbook/election/election.lua
+++ cookbook/election/election.lua
@@ -2,24 +2,38 @@
 -- election.lua
 -- An instance file for the three instances running leader election
 -- based on RAFT together with synchronous replication.
--- To run the cluster create three identical files:
--- election1.lua, election2.lua, election3.lua
--- Run 3 tarantool instances from corresponding directories:
--- `tarantool election1.lua`
--- `tarantool election2.lua`
--- `tarantool election3.lua`
 --
--- Determine this instance's id based on filename:
--- election1.lua -> 1
--- election2.lua -> 2
--- and so on.
-local instance_id = string.match(arg[0], "%d")
+
+-- Check tarantool version.
+assert(_TARANTOOL >= '2.6.1', 'tarantool 2.6.1+ required')
+
+local instance_id = string.match(arg[1], '^%d+$')
+assert(instance_id, 'malformed instance id')
+
 -- This instance's listening port.
 local port = 3300 + instance_id
+-- Thid instance's working directory.
+local workdir = 'tnt'..instance_id
+local fio = require('fio')
+if not fio.path.exists(workdir) then
+    local ok, err = fio.mkdir(workdir)
+    assert(ok, "Failed to create working directory "..workdir)
+end
+
+local fiber = require('fiber')
 
 box.cfg{
-    listen=port,
-    replication = {3301,3302,3303},
+    wal_dir=workdir,
+    memtx_dir=workdir,
+
+    instance_uuid='aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaa'..instance_id,
+
+    listen='127.0.0.1:'..port,
+    replication = {
+        '127.0.0.1:'..3301,
+        '127.0.0.1:'..3302,
+        '127.0.0.1:'..3303,
+    },
     replication_connect_quorum=0,
 
     -- The instance is set to candidate, so it may become leader itself
@@ -36,13 +50,17 @@ box.cfg{
     -- rolled back if no quorum is achieved during 1 second.
     replication_synchro_timeout=1,
     -- Heartbeat timeout. A leader is considered dead if it doesn't
-    -- send heartbeats for 4 * replication_timeout (4 seconds in our case).
+    -- send heartbeats for 4 * replication_timeout (1 second in our case).
     -- Once the leader is dead, remaining instances start a new election round.
-    replication_timeout=1,
+    replication_timeout=0.25,
     -- Timeout between elections. Needed to restart elections when no leader
     -- emerges soon enough.
-    election_timeout=1,
+    election_timeout=0.25,
 }
+
+function api_insert(key)
+    return box.space.test:insert{fiber.time64() / 1e4, key}
+end
 
 box.once('bootstrap', function()
     -- Grant the guest user replication rights.
@@ -51,7 +69,11 @@ box.once('bootstrap', function()
     -- Create a synchronous space to enable synchronous replication
     -- for transactions touching this space.
     box.schema.space.create('test', {is_sync=true})
-    box.space.test:create_index('pk')
+    box.space.test:format({
+        {name='timestamp', type='scalar'},
+        {name='iteration', type='unsigned'}
+    })
+    box.space.test:create_index('pk', {unique=true, parts={'timestamp', 'iteration'}})
 
     -- Grant rights for the router connection.
     box.schema.user.grant('guest', 'write', 'space', 'test')
--- cookbook/election/router.lua
+++ cookbook/election/router.lua
@@ -5,95 +5,105 @@
 -- left alive instances, and continues forwarding requests to it.
 --
 
-netbox = require('net.box')
+local netbox = require('net.box')
 -- Log transactions and leader switches.
-log = require('log')
-fiber = require('fiber')
+local log = require('log')
+local fiber = require('fiber')
+
+local reconnect_interval = 0.1
+local insertion_interval = 1
 
 -- List of tarantool ports (ips) to connect to.
-ports = {
+local ports = {
     3301,
     3302,
     3303,
 }
 
--- A helper returning number of set elements in a table.
-function sizeof(tbl)
-    local c = 0
-    for k,v in pairs(tbl) do c = c + 1 end
-    return c
+local conns = {}
+local leader_conn = nil
+local leader_no = nil
+
+local function connect(port)
+    local conn = netbox.connect(port)
+    if conn.state ~= 'error' then
+        log.info('Connection to instance at '..port..' established')
+        return conn
+    end
+    return nil
 end
 
--- Try to establish/reestablish connections to all the instances
--- listed in ports table.
-function establish_connection(conns)
-    log.info('Establishing connections...')
-    for k, port in pairs(ports) do
-        if conns[k] == nil then
-            local conn = netbox.connect(port)
-            if conn.state ~= 'error' then
-                conns[k] = conn
-                log.info('Connection to instance at '..port..' established')
-            else
-                log.info('Failed to connect to instance at '..port)
-            end
+local function connection(id, port)
+    conns[id] = connect(port)
+    while true do
+        fiber.sleep(reconnect_interval)
+        if conns[id] == nil then
+            conns[id] = connect(port)
         end
     end
-    log.info('Connected to '..sizeof(conns)..' instances.')
 end
 
--- Return a connection to an instance currently being the cluster leader.
-function find_leader(conns)
-    log.info('Looking for a new leader')
-    local leader_conn = nil
-    while not leader_conn do
+local function set_leader(conns)
+    while true do
+        if leader_conn ~= nil then
+            goto continue
+        end
         for k, conn in pairs(conns) do
-            -- Determine leadership based on `box.info.election.state` output.
             local ok, is_leader = pcall(conn.eval, conn,
-                                  'return box.info.election.state == \'leader\'')
+                                        [[return box.info.election.state == 'leader']])
             if ok then
                 if is_leader then
                     leader_conn = conn
-                    goto continue
+                    leader_no = k
+                    log.info('Leader found at '..conn.port)
+                    break
                 end
             else
-                -- Connection to one of the instances is lost. Try to reconnect.
-                log.info('No connection to '..conn.port)
+                log.info('No connection to instance at '..conn.port)
                 conns[k] = nil
-                establish_connection(conns)
             end
         end
-        -- Have some rest. Do not spam instances constantly while election is
-        -- still in progress. None of them is leader yet.
-        fiber.sleep(0.1)
         ::continue::
+        fiber.sleep(reconnect_interval)
     end
-    log.info('New leader found at '..leader_conn.port)
-    return leader_conn
+end
+
+local function wait_leader()
+    while leader_conn == nil do fiber.sleep(0.01) end
+    return leader_no, leader_conn
 end
 
 -- Insert some data to a synchronous space every now and then.
 -- Once the leader dies, wait for a new leader to emerge and
 -- continue relaying requests to it.
-function  generate_data(conns)
-    local leader_conn = find_leader(conns)
-    for i = 1,1000 do
+local function  generate_data()
+    local _, l_conn = wait_leader()
+    for i = 1, 1000000 do
+        local st = fiber.time()
         ::retry::
-        local ok, err = pcall(leader_conn.space.test.insert,
-                              leader_conn.space.test, {i})
+        local ok, err = pcall(l_conn.call, l_conn, "api_insert", {i})
         log.info('test:insert{'..i..'} - '..(ok and 'success' or 'fail'))
         if not ok then
             log.info(err)
             -- Insertion failed. The leader is dead or reconfigured to voter.
             -- Wait for a new leader.
-            leader_conn = find_leader(conns)
+            leader_conn = nil
+            _, l_conn = wait_leader()
             goto retry
         end
-        fiber.sleep(3)
+        log.info('{'..i..'} took '..fiber.time()-st..' seconds')
+        fiber.sleep(insertion_interval)
     end
 end
 
-conns = {}
-establish_connection(conns)
-generate_data(conns)
+local workers = {}
+for k, port in pairs(ports) do
+    workers[k] = fiber.new(connection, k, port)
+end
+
+local leader_f = fiber.new(set_leader, conns)
+
+generate_data()
+
+os.exit()
 
